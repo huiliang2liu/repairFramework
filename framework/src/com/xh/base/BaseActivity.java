366cfbaf3fd6128e9269763b94f29b8cbf8640ce
liuhuiiang <825378291@qq.com>
2018-01-25 16:53:42 +0800

    Create BaseActivity.java

---
+++ framework/src/com/xh/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.xh.base;
+
+
+/**
+ * @version 创建时间：2017-12-23 下午6:03:32 项目：repair 包名：com.xh.base
+ *          文件名：BaseActivity.java 作者：lhl 说明:
+ */
+
+public class BaseActivity extends XhBaseActivity {
+}
