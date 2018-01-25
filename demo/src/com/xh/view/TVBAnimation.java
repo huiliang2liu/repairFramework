eb523e9e78a29074633df4add041099e4b0b994d
liuhuiiang <825378291@qq.com>
2018-01-25 18:20:15 +0800

    Add files via upload

--- demo/src/com/xh/view/TVBAnimation.java
+++ demo/src/com/xh/view/TVBAnimation.java
@@ -1,1 +1,47 @@
-a
+package com.xh.view;
+
+import android.view.View;
+import android.view.animation.Animation;
+
+/**
+ * @version 创建时间�?017-11-16 下午5:59:00
+ * 项目：TvBlackAD-eclipse
+ * 包名：com.tvblack.tv.animation
+ * 文件名：TVBAnimation.java
+ * 作�?：lhl
+ * 说明:动画
+ */
+
+public class TVBAnimation extends Animation {
+private boolean isFillAfter=true;
+private int time=700;
+/**
+ * 
+ * lhl
+ * 2017-11-16 下午6:01:57
+ * 说明：开始动�?
+ * @param view void
+ */
+public void start(View view){
+	if(view==null)
+		return;
+	this.setFillAfter(isFillAfter);
+	this.setDuration(time);
+	view.startAnimation(this);
+}
+public boolean isFillAfter() {
+	return isFillAfter;
+}
+public void setFillAfter(boolean isFillAfter) {
+	this.isFillAfter = isFillAfter;
+}
+public int getTime() {
+	return time;
+}
+public void setTime(int time) {
+	if(time<=0)
+		time=700;
+	this.time = time;
+}
+
+}
---
+++ demo/src/com/xh/view/TVBTranslateAnimation.java
@@ -0,0 +1,54 @@
+package com.xh.view;
+
+import android.view.animation.Transformation;
+
+/**
+ * @version 创建时间�?017-11-16 下午6:09:50
+ * 项目：TvBlackAD-eclipse
+ * 包名：com.tvblack.tv.animation
+ * 文件名：TVBTranslateAnimation.java
+ * 作�?：lhl
+ * 说明:位置变化动画
+ */
+
+public class TVBTranslateAnimation extends TVBAnimation {
+private float mFromXDelta=0;
+private float mToXDelta=0;
+private float mFromYDelta=0;
+private float mToYDelta=0;
+public float getmFromXDelta() {
+	return mFromXDelta;
+}
+public void setmFromXDelta(float mFromXDelta) {
+	this.mFromXDelta = mFromXDelta;
+}
+public float getmToXDelta() {
+	return mToXDelta;
+}
+public void setmToXDelta(float mToXDelta) {
+	this.mToXDelta = mToXDelta;
+}
+public float getmFromYDelta() {
+	return mFromYDelta;
+}
+public void setmFromYDelta(float mFromYDelta) {
+	this.mFromYDelta = mFromYDelta;
+}
+public float getmToYDelta() {
+	return mToYDelta;
+}
+public void setmToYDelta(float mToYDelta) {
+	this.mToYDelta = mToYDelta;
+}
+@Override
+	protected void applyTransformation(float interpolatedTime, Transformation t) {
+		// TODO Auto-generated method stub
+	float dx=mFromXDelta;
+	float dy=mFromYDelta;
+	if(mFromXDelta!=mToXDelta)
+		dx=mFromXDelta+(mToXDelta-mFromXDelta)*interpolatedTime;
+	if(mFromYDelta!=mToYDelta)
+		dy=mFromYDelta+(mToYDelta-mFromYDelta)*interpolatedTime;
+	t.getMatrix().setTranslate(dx, dy);
+	}
+}
